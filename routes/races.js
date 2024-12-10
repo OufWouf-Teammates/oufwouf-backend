99e5fa866a794f9637d23fefed59b1c764ac7b58
Your name <Your email>
2024-12-10 15:02:55 +0100

    changement route race

--- routes/races.js
+++ routes/races.js
@@ -1,15 +1,15 @@
-var express = require("express")
-var router = express.Router()
-const Race = require("../models/race")
+var express = require("express");
+var router = express.Router();
+const Race = require("../models/race");
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  const pattern = new RegExp(req.query.search, "i")
-  const racesData = await Race.find({ name: pattern }).limit(5)
+  const pattern = new RegExp(req.query.search, "i");
+  const racesData = await Race.find({ name: pattern }).limit(5).lean();
 
-  const races = racesData.sort()
+  const races = racesData.sort();
 
-  res.json({ data: races })
-})
+  res.json({ data: races });
+});
 
-module.exports = router
+module.exports = router;
