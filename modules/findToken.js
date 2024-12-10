ef3f3ca76c0823e4c1e15d8e92b9d8e906d8a765
Paul Hatte <paul.hatte@gmail.com>
2024-12-10 10:36:09 +0100

    modifications légères route users et intégration du middleware à un autre fichier et création dune fonction pour retrouver le token

---
+++ modules/findToken.js
@@ -0,0 +1,10 @@
+function findToken(req) {
+    const tokenHere = req.headers.authorization?.split(' ')[1];
+   if(tokenHere) {
+    return tokenHere;
+   }
+   return false;
+  };
+  
+
+  module.exports = { findToken };
--- routes/users.js
+++ routes/users.js
@@ -11,45 +11,8 @@ const TOKEN_VALIDITY_DAYS = 90;
 const fs = require('fs');
 const path = require('path');
 
-
-
-
-//ROUTE MIDDLEWARE VERIF VALIDITE TOKEN / DATE TOKEN
-async function middlewareCheckToken(req, res, next) {
-  try {
-    // Récupérer le token depuis l'en-tête Authorization
-    const token = req.headers.authorization?.split(' ')[1]; // Format "Bearer <token>"
-
-    if (!token) {
-      res.json({ result: false, error: 'Token error.' });
-      return;
-        }
-
-    // Rechercher l'utilisateur associé au token
-    const user = await User.findOne({ token });
-    if (!user) {
-      res.json({ result: false, error: 'Token error.' });
-      return;
-      }
-
-    // Vérifier si le token est toujours valide (moins de 90 jours)
-    const tokenCreationDate = new Date(user.tokenCreationDate);
-    const currentDate = new Date();
-    const daysSinceCreation = Math.floor((currentDate - tokenCreationDate) / (1000 * 60 * 60 * 24));
-
-    if (daysSinceCreation > TOKEN_VALIDITY_DAYS) {
-      res.json({ result: false, error: 'Token error.' });
-      return;
-    }
-
-    // Si tout est valide, retourner une réponse ou passer au prochain middleware
-    next();
-  } catch (error) {
-    res.json({ result: false, error: 'Token error.' });
-    return;
-  }
-};
-
+const { middlewareCheckToken } = require('../modules/middlewareCheckToken');
+const { findToken } = require('../modules/findToken');
 
 
 //Route POST SIGNUP pour l'inscription
@@ -70,7 +33,8 @@ router.post('/signup', (req, res) => {
         password: hash,
         bookmarks: [],
         token: uid2(32),
-        tokenCreationDate : new Date()
+        tokenCreationDate : new Date(),
+        dogs: []
       });
 
       newUser.save().then(data => {
@@ -163,10 +127,10 @@ router.delete('/:token', async (req, res) => {
 
 // ROUTE APPLE CONNECTION 
 // Charger la clé privée depuis le fichier .p8
+router.post('/api/auth/apple', async (req, res) => {
 const pathToKey = path.join(__dirname, '..', 'config', 'AuthKey_GV7A3M6663.p8');
 const privateKey = fs.readFileSync(pathToKey, 'utf8');
 
-router.post('/api/auth/apple', async (req, res) => {
   const { token } = req.body;
 
   try {
