3a7d7e350eb46e7db977c4b14bcb6b1e3c5d6e37
Your name <Your email>
2024-12-11 15:00:57 +0100

    picture

--- routes/personalPicture.js
+++ routes/personalPicture.js
@@ -1,28 +1,41 @@
-var express = require("express")
-var router = express.Router()
-var Dog = require("../models/dog")
-var User = require("../models/user")
-var Picture = require("../models/personalPicture")
+var express = require("express");
+var router = express.Router();
+var Dog = require("../models/dog");
+var User = require("../models/user");
+var Picture = require("../models/personalPicture");
 
 /* Route pour afficher les photos */
 
-const { middlewareCheckToken } = require("../modules/middlewareCheckToken")
-var { upload } = require("../modules/cloudinary")
+const { middlewareCheckToken } = require("../modules/middlewareCheckToken");
+var { upload } = require("../modules/cloudinary");
 
-router.get('/', middlewareCheckToken, async (req, res, next) => {
-    const token = req.headers.authorization
+router.get("/", middlewareCheckToken, async (req, res, next) => {
+  const token = req.headers.authorization;
 
-    try {
-        const user = await User.findOne({ token: token }).populate("pictures")
-        res.json({ result: true, dog: user.pictures })
-    } catch (error){
-        console.error(error)
-        res.status(500).json({ result: false, error: "erreur serveur" })
-    }
-})
+  try {
+    const user = await User.findOne({ token: token }).populate("pictures");
+    res.json({ result: true, dog: user.pictures });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ result: false, error: "erreur serveur" });
+  }
+});
 
 /* Route pour ajouter une photo */
 
-router.post('/', middlewareCheckToken, upload, async (req, res, next) =>{
+router.post("/", middlewareCheckToken, upload, async (req, res, next) => {
+  try {
+    if (!token) {
+      return res.status(401).json({ result: false, error: "Token manquant" });
+    }
+    if (!req.body.data || !req.files) {
+      return res
+        .status(400)
+        .json({ result: false, error: "Données invalides" });
+    }
     
-})
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ result: false, error: "erreur serveur" });
+  }
+});
