87802e31385ddaa827bc361828ac9a0f9377cb0e
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-13 16:55:39 +0100

    modification de routes vaccins

--- routes/vaccinsPersos.js
+++ routes/vaccinsPersos.js
@@ -5,25 +5,40 @@ const User = require("../models/user")
 
 /* GET home page. */
 router.post("/", async (req, res, next) => {
-  const newVaccin = new VaccinPerso({
-    name: req.body.name,
-    rappel: req.body.rappel,
-    date: req.body.date,
-  })
+  try {
+    // Créez le vaccin personnalisé
+    const newVaccin = new VaccinPerso({
+      name: req.body.name,
+      rappel: req.body.rappel,
+      date: req.body.date,
+    });
 
-  const save = await newVaccin.save()
+    const savedVaccin = await newVaccin.save();
 
-  const user = await findOne({ token: req.headers.authorization }).populate(
-    "dogs"
-  )
+    // Trouvez l'utilisateur associé au token
+    const user = await User.findOne({ token: req.headers.authorization }).populate("dogs");
 
-  const dogId = user.dogs[0]._id
-  await User.updateOne(
-    { "dogs._id": dogId },
-    { $push: { "dogs.$.vaccins": save._id } }
-  )
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur introuvable" });
+    }
 
-  res.json({ data: save })
-})
+    // Vérifiez si l'utilisateur a des chiens
+    const dogId = user.dogs[0]?._id;
+    if (!dogId) {
+      return res.status(404).json({ error: "Aucun chien trouvé pour cet utilisateur" });
+    }
+
+    // Ajoutez l'ID du vaccin au tableau `vaccins` du chien
+    await User.updateOne(
+      { "dogs._id": dogId },
+      { $push: { "dogs.$.vaccins": savedVaccin._id } }
+    );
+
+    res.json({ data: savedVaccin });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erreur interne du serveur" });
+  }
+});
 
 module.exports = router
