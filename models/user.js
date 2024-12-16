0ca674a52948055d1e7828e9798412b127ab1fa1
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-16 12:30:00 +0100

    mise en place de test user, route user pour modifier les emails

--- models/user.js
+++ models/user.js
@@ -1,18 +1,5 @@
 const mongoose = require("mongoose")
 
-<<<<<<< HEAD
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  bookmarks: Array,
-  token: String,
-  idApple: String,
-  tokenCreationDate: Date,
-  dogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Dog" }],
-  pictures: [{ type: mongoose.Schema.Types.ObjectId, ref: "Picture" }],
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Favorite" }],
-})
-=======
   const userSchema = new mongoose.Schema({
     email: String,
     password: String,
@@ -24,7 +11,6 @@ const userSchema = new mongoose.Schema({
     pictures: [{ type: mongoose.Schema.Types.ObjectId, ref: "Picture" }],
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "favorites" }]
   });
->>>>>>> 4ecd780017da98ac22f2f372bac39f95e34b0eea
 
 const User = mongoose.model("users", userSchema)
 
