be72746465f3bce45f097ec408433ac28b93d7ed
Raphael <raphael.chagny@gmail.com>
2024-12-16 13:47:32 +0100

    s

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   tokenCreationDate: Date,
   dogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Dog" }],
   pictures: [{ type: mongoose.Schema.Types.ObjectId, ref: "Picture" }],
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "favorites" }],
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Favorite" }],
 })
 
 const User = mongoose.model("users", userSchema)
