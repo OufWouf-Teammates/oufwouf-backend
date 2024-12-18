79dee0f490493a246b4737b6567b98e3fc434975
Raphael <raphael.chagny@gmail.com>
2024-12-18 10:06:49 +0100

    voila

--- models/room.js
+++ models/room.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose")
 const doogSchema = new mongoose.Schema({
   name: { type: String, required: true },
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "messages" }],
 })
 
-const Room = mongoose.model("Message", messageSchema)
+const Room = mongoose.model("messages", messageSchema)
 
 module.exports = Room
