b566a347426c6f0c0ee30ad2aa04d8bef3c8ffaf
Your name <Your email>
2024-12-19 10:06:26 +0100

    route friends

--- routes/friends.js
+++ routes/friends.js
@@ -1,121 +1,125 @@
-var express = require("express")
-var router = express.Router()
-var Dog = require("../models/dog")
-var User = require("../models/user")
-const Room = require("../models/room")
+var express = require("express");
+var router = express.Router();
+var Dog = require("../models/dog");
+var User = require("../models/user");
+const Room = require("../models/room");
 
-const { middlewareCheckToken } = require("../modules/middlewareCheckToken")
-const Friend = require("../models/friend")
+const { middlewareCheckToken } = require("../modules/middlewareCheckToken");
+const Friend = require("../models/friend");
 
 router.post("/request/:token", async (req, res, next) => {
-    const { token } = req.params;
+  const { token } = req.params;
 
   try {
     if (!token) {
-      return res.status(401).json({ result: false, error: " token pas trouvé" })
+      return res
+        .status(401)
+        .json({ result: false, error: " token pas trouvé" });
     }
 
-    const sender = await User.findOne({ token })
-    const receiver = req.body.receiverId
+    const sender = await User.findOne({ token });
+    const receiver = req.body.receiverId;
 
     const verify = await Friend.findOne({
       from: sender._id,
       to: receiver,
       status: "pending",
-    })
+    });
 
     if (verify) {
       return res
         .status(400)
-        .json({ result: false, error: "demande déjà envoyé" })
+        .json({ result: false, error: "demande déjà envoyé" });
     }
 
     const newFriend = new Friend({
       from: sender._id,
       to: receiver,
-    })
+    });
 
-    await newFriend.save()
+    await newFriend.save();
 
-    res.status(201).json({ message: "Demande envoyée!" })
+    res.status(201).json({ message: "Demande envoyée!" });
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ result: false, error: "Erreur serveur" })
+    console.error(error);
+    return res.status(500).json({ result: false, error: "Erreur serveur" });
   }
-})
+});
 
 router.post("/addFriend/:id", async (req, res, next) => {
-  const { id } = req.params
-  const { decision } = req.body
+  const { id } = req.params;
+  const { decision } = req.body;
 
-  const request = await Friend.findById(id)
-  console.log(decision)
+  const request = await Friend.findById(id);
+  console.log(decision);
   try {
     if (!request || request.status !== "pending") {
       return res
         .status(400)
-        .json({ result: false, error: "demande pas trouvée ou déjà traitée" })
+        .json({ result: false, error: "demande pas trouvée ou déjà traitée" });
     }
 
     if (decision === "accepted") {
-      request.status = "accepted"
-      await request.save()
+      request.status = "accepted";
+      await request.save();
 
       await User.findByIdAndUpdate(request.from, {
         $addToSet: { friendList: request.to },
-      })
+      });
       await User.findByIdAndUpdate(request.to, {
         $addToSet: { friendList: request.from },
-      })
+      });
 
       //Create room
 
       const user1 = await User.findOne({ _id: request.from }).populate({
         path: "friendList",
         populate: { path: "dogs", select: "name" },
-      })
+      });
 
       const user2 = await User.findOne({ _id: request.to }).populate({
         path: "friendList",
         populate: { path: "dogs", select: "name" },
-      })
+      });
 
       const newRoom = new Room({
         name: [user1.dogs[0].name, user2.dogs[0].name],
         users: [user1._id, user2._id],
         messages: [],
-      })
+      });
 
-      const room = await newRoom.save()
+      const room = await newRoom.save();
 
-      res.status(200).json({ message: "Demande acceptée.", room: room })
+      res.status(200).json({ message: "Demande acceptée.", room: room });
     } else if (decision === "rejected") {
-      request.status = "rejected"
-      await request.save()
-      res.status(200).json({ message: "Demande refusée." })
+      request.status = "rejected";
+      await request.save();
+      res.status(200).json({ message: "Demande refusée." });
     }
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ result: false, error: "Erreur serveur" })
+    console.error(error);
+    return res.status(500).json({ result: false, error: "Erreur serveur" });
   }
-})
+});
 
-router.get("/", middlewareCheckToken, async (req, res, next) => {
-  const { token } = req
+router.get("/allFriends/:token", async (req, res, next) => {
+  const { token } = req.params;
   try {
     const user = await User.findOne({ token }).populate({
       path: "friendList",
       populate: { path: "dogs", select: "name" },
-    })
+    });
     if (!user) {
-      return res.status(400).json({ result: false, error: "pas d'user trouvé" })
+      return res
+        .status(400)
+        .json({ result: false, error: "pas d'user trouvé" });
     }
     return res.status(201).json({ friends: user.friendList });
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ result: false, error: "Erreur serveur" })
+    console.error(error);
+    return res.status(500).json({ result: false, error: "Erreur serveur" });
   }
-})
+});
 
 // router.get("/requestList/:token", async (req, res, next) => {
 //   const { token } = req.params;
@@ -145,8 +149,8 @@ router.get("/", middlewareCheckToken, async (req, res, next) => {
 // });
 
 router.get("/requestList/:token", async (req, res, next) => {
-  const { token } = req.params
-  const sender = await User.findOne({ token })
+  const { token } = req.params;
+  const sender = await User.findOne({ token });
 
   try {
     const request = await Friend.find({
@@ -155,18 +159,18 @@ router.get("/requestList/:token", async (req, res, next) => {
     }).populate({
       path: "from",
       populate: { path: "dogs", select: "name uri" },
-    })
+    });
 
     if (request.length === 0) {
       return res
         .status(200)
-        .json({ result: false, error: "Aucune demande trouvée" })
+        .json({ result: false, error: "Aucune demande trouvée" });
     }
-    res.status(201).json({ requestList: request })
+    res.status(201).json({ requestList: request });
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ result: false, error: "Erreur serveur" })
+    console.error(error);
+    return res.status(500).json({ result: false, error: "Erreur serveur" });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
